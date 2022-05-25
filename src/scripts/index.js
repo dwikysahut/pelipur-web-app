2b56656e3ba8eeaf9ad3f890e59d29c24780bd1e
Nova Ramadhona <novaramadhona2000@gmail.com>
2022-05-25 20:30:06 +0700

    create aside

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,11 +1,12 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
+import '../styles/aside.css';
 import '../styles/responsive.css';
 
-const menuToggel = document.querySelector('.menu-toggle button');
-const drawerelement = document.querySelector('#drawer');
-menuToggel.addEventListener('click', function () {
-  drawerelement.classList.toggle('slide');
-});
+// const menuToggel = document.querySelector('.menu-toggle button');
+// const drawerelement = document.querySelector('#drawer');
+// menuToggel.addEventListener('click', () => {
+//   drawerelement.classList.toggle('slide');
+// });
 
 console.log('Hello Coders! :)');
