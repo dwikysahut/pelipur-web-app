ff41fb7be49c5030b41604cf9723958821c2cf25
yogatresnaa <yogatresnaa@gmail.com>
2022-05-29 09:18:37 +0700

    test minor

--- src/scripts/views/pages/beranda.js
+++ src/scripts/views/pages/beranda.js
@@ -1,7 +1,17 @@
+import data from '../../../DATA.json';
+
 const Beranda = {
   async render() {
     return `
+    <style>
+    h2{
+      color:red;
+      padding:20px;
+    }
+    </style>
+
         <h2>beranda</h2>
+        <h3>Berita</h3>
         `;
   },
 
