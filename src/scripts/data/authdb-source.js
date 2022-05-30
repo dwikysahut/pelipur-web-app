fc930ac7f5eb111fda6c16b5ce5db56c1c56a8dc
Dwiky Satria Hutomo <dwikysahut@gmail.com>
2022-05-30 22:03:10 +0700

    finish authentication features and connected with backend

--- src/scripts/data/authdb-source.js
+++ src/scripts/data/authdb-source.js
@@ -3,11 +3,24 @@ import API_ENDPOINT from '../globals/api-endpoint';
 
 class AuthDbSource {
   static async postLogin(body) {
-    console.log(API_ENDPOINT.LOGIN);
-    const response = await axios.post('http://localhost:5000/auth/login', body);
-
+    const response = await axios.post(API_ENDPOINT.LOGIN, body);
     return response;
     // const response =
+  }
+
+  static async postRegister(body) {
+    const response = await axios.post(API_ENDPOINT.REGISTER, body);
+    return response;
+  }
+
+  static async postVerifyEmail(body) {
+    const response = await axios.post(API_ENDPOINT.VERIFY_EMAIL, body);
+    return response;
+  }
+
+  static async postForgotPassword(body) {
+    const response = await axios.post(API_ENDPOINT.FORGOT_PASSWORD, body);
+    return response;
   }
 }
 
