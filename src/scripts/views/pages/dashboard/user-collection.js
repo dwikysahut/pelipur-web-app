027c721860111ab5a1edefc680a4e7a0155996bc
devaem <devaega13@gmail.com>
2022-05-30 15:03:45 +0700

    added aside-user, fix some code

--- src/scripts/views/pages/dashboard/user-collection.js
+++ src/scripts/views/pages/dashboard/user-collection.js
@@ -1,10 +1,10 @@
-import '../../component/aside-dashboard';
+import '../../component/aside-user';
 
 const UserCollection = {
   async render() {
     return `
     <div class="wrapper-dashboard">
-      <aside-dashboard></aside-dashboard>
+      <aside-user></aside-user>
     
       <div class="dashboard-container">
         <div class="content">
