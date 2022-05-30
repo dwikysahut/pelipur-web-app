4632377493dd44414f1a80a8b67734efd0a45797
devaem <devaega13@gmail.com>
2022-05-30 14:15:41 +0700

    added user collection and user history page, fix some element

--- src/scripts/views/pages/auth/forgot-password.js
+++ src/scripts/views/pages/auth/forgot-password.js
@@ -14,12 +14,12 @@ const ForgotPassword = {
                   <h2>Verifikasi email</h2>
                   <p>Masukkan <b>Email</b> anda, password baru akan dikirim ke <b>Email</b> Anda</p>
                   <div class="field">
-                    <input type="email" id="inputEmailForgot" name="inputEmailForgot" placeholder="Masukan Kode" required>
+                    <input type="email" id="inputEmailForgot" name="inputEmailForgot" placeholder="Masukan Password Baru" required>
                   </div>
                   
                   <div class="field btn">
                     <div class="btn-layer"></div>
-                      <input type="submit" id="submitForgotPassword"  value="Kirim">
+                      <input type="submit" id="submitForgotPassword"  value="Masuk">
                   </div>
                 </form>
         
